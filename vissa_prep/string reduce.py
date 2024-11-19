309aae2ad9d677a80295290914a129ed22b6f43a
Vinuthna <142153887+Vin-thna@users.noreply.github.com>
2024-11-19 11:15:33 +0530

    Create string reduce.py

---
+++ vissa_prep/string reduce.py
@@ -0,0 +1,13 @@
+def rs(s):
+    res=""
+    c=1
+    for i in range(1,len(s)):
+        if s[i]==s[i-1]:
+            c+=1
+        else:
+            res+=s[i-1]+str(c)
+            c=1
+    res+=s[-1]+str(c)
+    return res
+s=input().strip()
+print(rs(s))
